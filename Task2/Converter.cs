2d570a9e00dd35a5fd7e4bfad747c136e492f15b
Lyam <l9m_pam@live.ru>
2020-02-02 19:18:19 +0300

    1. Исправлена ошибка конвертация из зарубежной валюты в рубль; 2. Добавлена возможность повторить цикл конвертации; 3. Добавлена цветовая легенда; 4. Убраны лишние переменные.

--- Task2/Converter.cs
+++ Task2/Converter.cs
@@ -13,8 +13,7 @@ namespace Task2
         //переменные для определения тяжести кошелька пользователя
         double ruble, dollar, euro, krone;
 
-        byte answerFromProgramcs;
-
+        //Пользовательский конструктор с тремя аргументами, содержащими курсы валют
         public Converter(double rubleToUsdRate, double rubleToEuroRate, double rubleToDkkRate)
         {
             this.rubleToUsdRate = rubleToUsdRate;
@@ -25,20 +24,95 @@ namespace Task2
         public double UsdToRubleRate { set { usdToRubleRate = value; } }
         public double EuroToRubleRate { set { euroToRubleRate = value; } }
         public double DkkToRubleRate { set { dkkToRubleRate = value; } }
-        public double Ruble { set { ruble = value; } }
-        public double Dollar { set { dollar = value; } }
-        public double Euro { set { euro = value; } }
-        public double Krone { set { krone = value; } }
-        public byte AnswerFromProgramcs { set { answerFromProgramcs = value; } }
 
+        //Получаем леенду и курс валют
+        void GetLegentAndCashRate()
+        {
+            Console.WriteLine("Легенда");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("■ - курс валют");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("■ - наличка в кошельке");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("■ - конвертация");
+            Console.ResetColor();
+
+            Console.WriteLine("\nКурс (покупка / продажа):");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("доллар\t{0} / {1}\nевро\t{2} / {3}\nкрона\t{4} / {5}\n", usdToRubleRate, rubleToUsdRate, euroToRubleRate, rubleToEuroRate, dkkToRubleRate, rubleToDkkRate);
+            Console.ResetColor();
+        }
+
+        //Устанавливаем количество налички в кошельке
+        public void SetTheAmountOfMoney()
+        {
+            GetLegentAndCashRate();
+
+            Console.WriteLine("В кошельке было:");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            for (int i = 0; i < 4; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        Console.Write("рублей = ");
+                        ruble = Double.Parse(Console.ReadLine());
+                        break;
+                    case 1:
+                        Console.Write("долларов = ");
+                        dollar = Double.Parse(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.Write("евро = ");
+                        euro = Double.Parse(Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.Write("датских крон = ");
+                        krone = Double.Parse(Console.ReadLine());
+                        break;
+                }
+            }
+            Console.ResetColor();
+        }
+
+        //Определяет конечное количество денег в кошельке
+        void DetermineTheAmountOfMoney()
+        {
+            Console.WriteLine("\nВ кошельке стало:");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("рублей = {0}\nдолларов = {1}\nевро = {2}\nкрон = {3}", ruble, dollar, euro, krone); ;
+            Console.ResetColor();
+        }
+
+        //Спрашивает: стоит ли повторить конвертацию (рубль в ... / ... в рубль)
+        void RepeatConversation()
+        {
+            Console.WriteLine("\nПовторим конвертацию денег");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("1 - да\n2 - нет");
+            Console.ResetColor();
+            Console.Write("Мой ответ = ");
+        }
+
+        //Производит конвертацию валюты (рубль в .../... в рубль)
         public void ConvertsCurrency()
         {
-            byte answerFromCase;
+            byte answerFromProgramcs, answerFromCase;
+
+            Console.WriteLine("\nВыберите перевод:");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("1 - рубль в ...\n2 - ... в рубль");
+            Console.ResetColor();
+            Console.Write("Мой ответ = ");
+            answerFromProgramcs = Byte.Parse(Console.ReadLine());
             switch (answerFromProgramcs)
             {
-                //добавить вовзращаемое значение в консоль
                 case 1:
-                    Console.Write("\n1 - рубль в доллар\n2 - рубль в евро\n3 - рубль в крону\n\nМой ответ = ");
+                    Console.WriteLine("\nРубль в ...");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("1 - ... доллар\n2 - ... евро\n3 - ... крону");
+                    Console.ResetColor();
+                    Console.Write("Мой ответ = ");
                     answerFromCase = Byte.Parse(Console.ReadLine());
                     if (answerFromCase == 1)
                     {
@@ -49,36 +123,41 @@ namespace Task2
                     {
                         euro += ruble / rubleToEuroRate;
                         ruble = 0;
-                    }                        
+                    }
                     else
                     {
                         krone += ruble / rubleToDkkRate;
                         ruble = 0;
-                    }                        
+                    }
                     break;
-
                 case 2:
-                    Console.Write("\n1 - доллар в рубль\n2 - евро в рубль\n3 - крона в рубль\n\nМой ответ = ");
+                    Console.WriteLine("\n... в рубль");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("1 - доллар ...\n2 - евро ...\n3 - крона ...");
+                    Console.ResetColor();
+                    Console.Write("Мой ответ = ");
                     answerFromCase = Byte.Parse(Console.ReadLine());
                     if (answerFromCase == 1)
                     {
                         ruble += dollar * usdToRubleRate;
                         dollar = 0;
-                    }         
+                    }
                     else if (answerFromCase == 2)
                     {
-                        ruble += euro / euroToRubleRate;
+                        ruble += euro * euroToRubleRate;
                         euro = 0;
-                    }                        
+                    }
                     else
                     {
-                        ruble += krone / dkkToRubleRate;
+                        ruble += krone * dkkToRubleRate;
                         krone = 0;
-                    }                        
+                    }
                     break;
                 default:
                     break;
             }
+            DetermineTheAmountOfMoney();
+            RepeatConversation();
         }
     }
 }
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -13,53 +13,22 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            double ruble = 0, dollar = 0, euro = 0, krone = 0;
             const double rubleToUsdRate = 65.03, rubleToEuroRate = 72.08, rubleToDkkRate = 100.80, usdToRubleRate = 62.84, euroToRubleRate = 69.63, dkkToRubleRate = 88.2;
-            
-            Console.WriteLine("Курс (покупка / продажа):\nдоллар\t{0} / {1}\nевро\t{2} / {3}\nкрона\t{4} / {5}\n", usdToRubleRate, rubleToUsdRate, euroToRubleRate, rubleToEuroRate, dkkToRubleRate, rubleToDkkRate);
 
-            DetermineTheAmountOfMoney();
-            
-            Converter conversion = new Converter(rubleToUsdRate, rubleToEuroRate, rubleToDkkRate);
-            conversion.UsdToRubleRate = usdToRubleRate;
-            conversion.EuroToRubleRate = euroToRubleRate;
-            conversion.DkkToRubleRate = dkkToRubleRate;
-            conversion.Ruble = ruble;
-            conversion.Dollar = dollar;
-            conversion.Euro = euro;
-            conversion.Krone = krone;
-
-            //секция if
-            Console.Write("\nВыберите перевод: \n1 - рубль в ...\n2 - ... в рубль\n\nМой ответ = ");
-            conversion.AnswerFromProgramcs = Byte.Parse(Console.ReadLine());
-            //добавить вовзращаемое значение в консоль
-
-            void DetermineTheAmountOfMoney()
+            Converter conversion = new Converter(rubleToUsdRate, rubleToEuroRate, rubleToDkkRate)
             {
-                Console.WriteLine("Раскройте бумажник и укажите количество:");
-                for (int i = 0; i < 4; i++)
-                {
-                    switch (i)
-                    {
-                        case 0:
-                            Console.Write("рублей = ");
-                            ruble = Double.Parse(Console.ReadLine());
-                            break;
-                        case 1:
-                            Console.Write("долларов = ");
-                            dollar = Double.Parse(Console.ReadLine());
-                            break;
-                        case 2:
-                            Console.Write("евро = ");
-                            euro = Double.Parse(Console.ReadLine());
-                            break;
-                        case 3:
-                            Console.Write("датских крон = ");
-                            krone = Double.Parse(Console.ReadLine());
-                            break;
-                    }
-                }
+                UsdToRubleRate = usdToRubleRate,
+                EuroToRubleRate = euroToRubleRate,
+                DkkToRubleRate = dkkToRubleRate,
+            };
+
+            conversion.SetTheAmountOfMoney();
+
+            do
+            {
+                conversion.ConvertsCurrency();
             }
+            while (Double.Parse(Console.ReadLine()) == 1);
 
             Console.ReadKey();
         }
